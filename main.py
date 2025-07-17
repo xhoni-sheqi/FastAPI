34ac926330c6a83a96a7a3ea4eb90f97618a4d6c
xhoni-sheqi <xhonisheqi.lavoro@gmail.com>
2025-07-17 17:14:50 +0200

    multiple post saved in array

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
-
+from random import randrange
 # calling app is a convention
 app = FastAPI()
 
@@ -16,6 +16,11 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+
+# save post [ no db ]
+
+my_posts = []
+
 #decorate
 @app.get("/")
 #plain python function - name the function as descriptive as possible
@@ -25,15 +30,15 @@ def root():
  
 @app.get("/posts")
 def get_posts():
-    return {"data": "First Post"} 
+    return {"data": my_posts} 
 
-@app.post("/createposts")
+@app.post("/posts")
 # auto extract the content
-def create_post(post: Post): 
-    print(post)
-    # you can use .dict() 
-    print(post.dict())
-    return {"message": "Post created" , "new_post": post}
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0,9999999)
+    my_posts.append(post_dict) 
+    return {"data" : post_dict}
 
 
 # title -> str and content -> str , we can include wherether we want 
