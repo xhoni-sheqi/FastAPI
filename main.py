4174ea3e93b69bc64dcc537a47443414518ad01a
xhoni-sheqi <xhonisheqi.lavoro@gmail.com>
2025-07-17 17:25:34 +0200

    added single searc on specific id

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI 
+from fastapi import FastAPI , HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -20,6 +20,11 @@ class Post(BaseModel):
 # save post [ no db ]
 
 my_posts = []
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
+    return None
 
 #decorate
 @app.get("/")
@@ -40,5 +45,10 @@ def create_post(post: Post):
     my_posts.append(post_dict) 
     return {"data" : post_dict}
 
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(int(id))
+    return {"post": post}
 
 # title -> str and content -> str , we can include wherether we want 
