94d4421cd404ac162e94e6016333ef4e911483f8
xhoni-sheqi <xhonisheqi.lavoro@gmail.com>
2025-07-18 09:22:47 +0200

    added put operation

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class Post(BaseModel):
 # save post [ no db ]
 
 # array to save ( db is a better choice indeed )
-my_posts = [{"title": "a", "content": "b", "published" : True , "rating": "123", "id" : 1},{"title": "a", "content": "b", "published" : True , "rating": "123", "id" : 2}]
+my_posts = [{"title": "a", "content": "b", "published" : True , "rating":123, "id" : 1},{"title": "a", "content": "b", "published" : True , "rating": 12, "id" : 2}]
 #search for a single post by id
 def find_post(id):
     for p in my_posts:
@@ -81,5 +81,15 @@ def delete_post(id: int):
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
     
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} doesn't exist ")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
 
 # title -> str and content -> str , we can include wherether we want 
